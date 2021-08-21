9d742a4105e1e0a2725ed27e8a66cfe4b913fbd9
Uwe L. Korn <uwe.korn@quantco.com>
2021-08-21 14:01:09 +0200

    Use rust compiler and modify wheel name

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,17 @@
-cd tensorboard/data/server
+#!/bin/bash
+
+set -euxo pipefail
+
+pushd tensorboard/data/server
 cargo build --release
-cd pip_package
-$PYTHON build.py --out-dir="$SRC_DIR/" --server-binary=../target/release/rustboard
+
+pushd pip_package
+$PYTHON build.py --out-dir="$SRC_DIR/" --server-binary=../target/${CARGO_BUILD_TARGET}/release/rustboard
+
+if [[ "${target_platform}" == "osx-arm64" ]]; then
+  WHEEL_NAME=$(ls $SRC_DIR/*.whl)
+  NEW_WHEEL_NAME=${WHEEL_NAME/macosx_10_9_x86_64/macosx_11_0_arm64}
+  mv $WHEEL_NAME ${NEW_WHEEL_NAME}
+fi
+
 $PYTHON -m pip install --no-deps --ignore-installed -v $SRC_DIR/*.whl
