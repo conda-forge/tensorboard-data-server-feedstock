df2d9e848a7bbc0a35875ac069bd03146390c6ea
ngam <67342040+ngam@users.noreply.github.com>
2023-02-10 23:08:10 -0500

    edit name??

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,7 +10,7 @@ $PYTHON build.py --out-dir="$SRC_DIR/" --server-binary=../target/${CARGO_BUILD_T
 
 if [[ "${target_platform}" == "osx-arm64" ]]; then
   WHEEL_NAME=$(ls $SRC_DIR/*.whl)
-  NEW_WHEEL_NAME=${WHEEL_NAME/macosx_10_9_x86_64/macosx_11_0_arm64}
+  NEW_WHEEL_NAME=${WHEEL_NAME/macosx_10_9_aarch64/macosx_11_0_arm64}
   mv $WHEEL_NAME ${NEW_WHEEL_NAME}
 fi
 
