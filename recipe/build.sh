013530f980e61819b962b884b2c3af834a07c133
Tobias Fischer <info@tobiasfischer.info>
2023-02-13 11:01:30 +1000

    Try passing flags to build script for osx-arm64

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,12 +6,14 @@ pushd tensorboard/data/server
 cargo build --release
 
 pushd pip_package
-$PYTHON build.py --out-dir="$SRC_DIR/" --server-binary=../target/${CARGO_BUILD_TARGET}/release/rustboard
 
 if [[ "${target_platform}" == "osx-arm64" ]]; then
-  WHEEL_NAME=$(ls $SRC_DIR/*.whl)
-  NEW_WHEEL_NAME=${WHEEL_NAME/macosx_10_9_aarch64/macosx_11_0_arm64}
-  mv $WHEEL_NAME ${NEW_WHEEL_NAME}
+  $PYTHON build.py --out-dir="$SRC_DIR/" --server-binary=../target/${CARGO_BUILD_TARGET}/release/rustboard --platform macosx_11_0 --cpu arm64
+  # WHEEL_NAME=$(ls $SRC_DIR/*.whl)
+  # NEW_WHEEL_NAME=${WHEEL_NAME/macosx_10_9_aarch64/macosx_11_0_arm64}
+  # mv $WHEEL_NAME ${NEW_WHEEL_NAME}
+else
+  $PYTHON build.py --out-dir="$SRC_DIR/" --server-binary=../target/${CARGO_BUILD_TARGET}/release/rustboard
 fi
 
 $PYTHON -m pip install --no-deps --ignore-installed -v $SRC_DIR/*.whl
