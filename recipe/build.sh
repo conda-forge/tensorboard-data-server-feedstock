d0dd36839112cb90e98a7b1e2751bc4be685ac97
Tobias Fischer <info@tobiasfischer.info>
2023-02-13 14:47:02 +1000

    Try reverse

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,7 +9,7 @@ pushd pip_package
 
 if [[ "${target_platform}" == "osx-arm64" ]]; then
   $PYTHON build.py --out-dir="$SRC_DIR/" --server-binary=../target/${CARGO_BUILD_TARGET}/release/rustboard --platform macosx_11_0 --cpu arm64
-  export SYSTEM_VERSION_COMPAT=0
+  export SYSTEM_VERSION_COMPAT=1
   $PYTHON -m pip install --no-deps --ignore-installed -v $SRC_DIR/*.whl
 else
   $PYTHON build.py --out-dir="$SRC_DIR/" --server-binary=../target/${CARGO_BUILD_TARGET}/release/rustboard
