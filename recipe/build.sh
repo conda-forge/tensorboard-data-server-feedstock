3877514daea64bb5ffbbba3c585bed9125fe1311
Tobias Fischer <info@tobiasfischer.info>
2023-02-13 13:33:23 +1000

    Try fix

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,8 +9,8 @@ pushd pip_package
 
 if [[ "${target_platform}" == "osx-arm64" ]]; then
   $PYTHON build.py --out-dir="$SRC_DIR/" --server-binary=../target/${CARGO_BUILD_TARGET}/release/rustboard --platform macosx_11_0 --cpu arm64
+  SYSTEM_VERSION_COMPAT=0 $PYTHON -m pip install --no-deps --ignore-installed -v $SRC_DIR/*.whl
 else
   $PYTHON build.py --out-dir="$SRC_DIR/" --server-binary=../target/${CARGO_BUILD_TARGET}/release/rustboard
+  $PYTHON -m pip install --no-deps --ignore-installed -v $SRC_DIR/*.whl
 fi
-
-$PYTHON -m pip install --no-deps --ignore-installed -v $SRC_DIR/*.whl
