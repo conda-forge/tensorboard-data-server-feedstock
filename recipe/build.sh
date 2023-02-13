ca6def1a102f995e5a185a84831f8d68947c147d
Tobias Fischer <info@tobiasfischer.info>
2023-02-13 11:26:53 +1000

    Debug

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,9 +9,6 @@ pushd pip_package
 
 if [[ "${target_platform}" == "osx-arm64" ]]; then
   $PYTHON build.py --out-dir="$SRC_DIR/" --server-binary=../target/${CARGO_BUILD_TARGET}/release/rustboard --platform macosx_11_0 --cpu arm64
-  # WHEEL_NAME=$(ls $SRC_DIR/*.whl)
-  # NEW_WHEEL_NAME=${WHEEL_NAME/macosx_10_9_aarch64/macosx_11_0_arm64}
-  # mv $WHEEL_NAME ${NEW_WHEEL_NAME}
 else
   $PYTHON build.py --out-dir="$SRC_DIR/" --server-binary=../target/${CARGO_BUILD_TARGET}/release/rustboard
 fi
