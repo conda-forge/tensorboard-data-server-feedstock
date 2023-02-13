2346ee7e4e5be6bd35ab6fe1cede60d78592e706
Tobias Fischer <info@tobiasfischer.info>
2023-02-13 15:13:16 +1000

    Bit nicer structure

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,10 +8,10 @@ cargo build --release
 pushd pip_package
 
 if [[ "${target_platform}" == "osx-arm64" ]]; then
-  $PYTHON build.py --out-dir="$SRC_DIR/" --server-binary=../target/${CARGO_BUILD_TARGET}/release/rustboard --platform macosx_11_0 --cpu arm64
-  export SYSTEM_VERSION_COMPAT=1
-  $PYTHON -m pip install --no-deps --ignore-installed -v $SRC_DIR/*.whl
+  export PLATFORM_FLAGS="--platform macosx_11_0 --cpu arm64"
 else
-  $PYTHON build.py --out-dir="$SRC_DIR/" --server-binary=../target/${CARGO_BUILD_TARGET}/release/rustboard
-  $PYTHON -m pip install --no-deps --ignore-installed -v $SRC_DIR/*.whl
+  export PLATFORM_FLAGS=""
 fi
+
+$PYTHON build.py --out-dir="$SRC_DIR/" --server-binary=../target/${CARGO_BUILD_TARGET}/release/rustboard $PLATFORM_FLAGS
+$PYTHON -m pip install --no-deps --ignore-installed -v $SRC_DIR/*.whl
