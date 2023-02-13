315481d5c1c4a08ade2bbe7b23a1a2c21f893c12
Tobias Fischer <info@tobiasfischer.info>
2023-02-13 15:13:40 +1000

    platform flags not needed with patch

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,11 +7,5 @@ cargo build --release
 
 pushd pip_package
 
-if [[ "${target_platform}" == "osx-arm64" ]]; then
-  export PLATFORM_FLAGS="--platform macosx_11_0 --cpu arm64"
-else
-  export PLATFORM_FLAGS=""
-fi
-
-$PYTHON build.py --out-dir="$SRC_DIR/" --server-binary=../target/${CARGO_BUILD_TARGET}/release/rustboard $PLATFORM_FLAGS
+$PYTHON build.py --out-dir="$SRC_DIR/" --server-binary=../target/${CARGO_BUILD_TARGET}/release/rustboard
 $PYTHON -m pip install --no-deps --ignore-installed -v $SRC_DIR/*.whl
